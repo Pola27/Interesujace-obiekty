e5c9e37b532bb522018ceacaae13039cf7b81124
Pola27 <paulina.r.nowak@gmail.com>
2015-11-28 15:07:57 +0100

    PA8

--- Nethack/Nethack/Nethack/Core/Game.cs
+++ Nethack/Nethack/Nethack/Core/Game.cs
@@ -9,23 +9,12 @@ namespace Nethack.Core
     class Game
     {
 
-      
+      {
 
-        private void generateBoard (int playersNumb, int obstacleNumb) {
-            for (int i=0;i<50;i++)
-                {
-                for(int j=0;j<50;j++)
-                        {
-                          
+       
 
 
-                        }
-
-
-                }
-
-
-       }
+        }
 
     }
 }
