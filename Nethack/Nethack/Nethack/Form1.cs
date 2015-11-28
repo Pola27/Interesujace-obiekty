87c5c240d1d06b012f56a32c7ee8579383121bba
nogr <nogr@ericpol.pl>
2015-11-28 18:26:35 +0100

    PM03

--- Nethack/Nethack/Nethack/Form1.cs
+++ Nethack/Nethack/Nethack/Form1.cs
@@ -20,7 +20,11 @@ namespace Nethack
             InitializeComponent();
 
             Game game = new Game();
+            Player player = new Player();
             GuiAccess guiAccess = new GuiAccess(this.pictureBox1);
+            Gameboard gameboard = new Gameboard(1,2);
+
+            guiAccess.RenderBoard(gameboard, player);
             //game.connect(this);
 
         }
@@ -46,5 +50,21 @@ namespace Nethack
             //turn off game
             Application.Exit();
         }
+
+
+        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case (char)49:
+                case (char)52:
+                case (char)55:
+                    MessageBox.Show("Form.KeyPress: '" +
+                        e.KeyChar.ToString() + "' consumed.");
+                    e.Handled = true;
+                    break;
+            }
+        }
+
     }
 }
